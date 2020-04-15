2ef0a9ef545b471274336a9da8c12b7904df0902
Jan Sernec <jan.sernec@gmail.com>
2020-04-15 22:34:37 +0200

    Updates

--- app.js
+++ app.js
@@ -37,10 +37,6 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
 
 
 // When the user scrolls the page, execute myFunction
