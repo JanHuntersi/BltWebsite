1ba856be99b89541aecbcbced9328e1c518946da
Jan Sernec <jan.sernec@gmail.com>
2020-04-17 12:26:09 +0200

    Updates

--- app.js
+++ app.js
@@ -22,19 +22,7 @@ const navSlide = () =>{
 }
 navSlide();
 
-//go to top button
-var mybutton = document.getElementById("gumb-za-navzgor");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 // When the user clicks on the button, scroll to the top of the document
 
